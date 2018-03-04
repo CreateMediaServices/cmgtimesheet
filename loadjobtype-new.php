c62f6571cd210ee5c4021845c60c1fb2c833f1dd
Shakeel <shakeel.rehman@createmedia-group.com>
2018-03-04 14:18:03 +0400

    Order for Job Type

--- loadjobtype-new.php
+++ loadjobtype-new.php
@@ -67,7 +67,7 @@ unset($row);
 unset($result);
 unset($totalRecords);
 
-$sql = "SELECT * FROM cmg_jobtypes WHERE deptID IN (".$dIDs.") AND clientID=".$clients;
+$sql = "SELECT * FROM cmg_jobtypes WHERE deptID IN (".$dIDs.") AND clientID=".$clients." ORDER BY jobTitle ASC";
 $result = $db->query( $sql );
 $totalRecords = $result->num_rows;
 $counter = 0;
